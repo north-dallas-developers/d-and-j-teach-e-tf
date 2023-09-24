77beb941c619f25b2f11c258565afcd6f1a6c889
Justin Williams <justinjohnwilliams@gmail.com>
2023-09-24 17:06:04 -0500

    01-00: our first vm

---
+++ code/lesson-01/00-hello-world/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+}
+
+output "id" {
+  value = aws_instance.example.id
+}
+
+output "state" {
+  value = aws_instance.example.instance_state
+}
