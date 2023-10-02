b208987113624cfa974177c186220c7ef68569b3
Justin Williams <justinjohnwilliams@gmail.com>
2023-10-02 08:39:30 -0500

    02-01: woohoo s3

---
+++ code/lesson-02/01-hello-s3/main.tf
@@ -0,0 +1,98 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# help ensure uniqueness
+resource "random_pet" "pet" {
+  length = 2
+}
+
+locals {
+  bucket_name = "${var.bucket_name}-${random_pet.pet.id}"
+  account_id  = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = local.bucket_name
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+data "aws_kms_alias" "s3" {
+  name = "alias/aws/s3"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_sse" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    bucket_key_enabled = false
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = data.aws_kms_alias.s3.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+
+  policy = data.aws_iam_policy_document.doc.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "doc" {
+  statement {
+    sid     = "EnforcedTLS"
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+  }
+  statement {
+    sid     = "RootAccess"
+    effect  = "Allow"
+    actions = ["s3:*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.account_id}:root"]
+    }
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
